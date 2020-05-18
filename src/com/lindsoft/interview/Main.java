7bf33d9263f3b092bb20628a9d74cba73868c21e
johanlind1984 <54557482+johanlind1984@users.noreply.github.com>
2020-05-18 13:35:31 +0200

    Initial commit

---
+++ src/com/lindsoft/interview/Main.java
@@ -0,0 +1,41 @@
+package com.lindsoft.interview;
+import java.util.Arrays;
+
+public class Main {
+    private static int thounsandBills = 2;
+    private static int fivhundredBills = 3;
+    private static int onehundredBills = 5;
+
+    public static void main(String[] args) {
+        int[] withdrawals = new int[]{1500, 700, 400, 1100, 1000, 700, 300};
+        Arrays.stream(withdrawals).forEach(sum ->
+                System.out.println("Withdrawing: " + sum + ", " + withdraw(sum, thounsandBills, fivhundredBills, onehundredBills)));
+    }
+
+    public static boolean withdraw(int amountToWithdrawLeft, int thousandBillsLeft, int fivehundredBillsLeft, int onehundredBillsLeft) {
+        if (amountToWithdrawLeft == 0) {
+            thounsandBills = thousandBillsLeft;
+            fivhundredBills = fivehundredBillsLeft;
+            onehundredBills = onehundredBillsLeft;
+            return true;
+        }
+
+        if (amountToWithdrawLeft >= 1000 && thousandBillsLeft > 0) {
+            return withdraw(amountToWithdrawLeft - 1000, thousandBillsLeft - 1, fivehundredBillsLeft, onehundredBillsLeft);
+        } else if (amountToWithdrawLeft >= 500 && fivehundredBillsLeft > 0) {
+            return withdraw(amountToWithdrawLeft - 500, thousandBillsLeft, fivehundredBillsLeft - 1, onehundredBillsLeft);
+        } else if (amountToWithdrawLeft >= 100 && onehundredBillsLeft > 0) {
+            return withdraw(amountToWithdrawLeft - 100, thousandBillsLeft, fivehundredBillsLeft, onehundredBillsLeft - 1);
+        }
+        return false;
+    }
+
+//    private static void handOutTheCash(int thounsandBillsToHand, int fivhundredBillsToHand, int onehundredBillsToHand) {
+//        System.out.println("WITDRAWAL SUCCESS");
+//        System.out.println(thounsandBillsToHand + ", 1000 Bills");
+//        System.out.println(fivhundredBillsToHand + ", 500 Bills");
+//        System.out.println(onehundredBillsToHand + ", 100 Bills");
+//        System.out.print("TOTAL: ");
+//        System.out.println(thounsandBillsToHand * 1000 + fivhundredBillsToHand * 500 + onehundredBillsToHand * 100 + "\n");
+//    }
+}
